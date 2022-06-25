92b32ef1aa06a977035e6046a2815e3af10c2073
B. Piatek <piatekbart@gmail.com>
2022-06-25 09:42:18 +0200

    initial commit

---
+++ src/main/java/pl/bpiatek/exerciseapp/ExerciseappApplication.java
@@ -0,0 +1,12 @@
+package pl.bpiatek.exerciseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExerciseappApplication.class, args);
+  }
+}
